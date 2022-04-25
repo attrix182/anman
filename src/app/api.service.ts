36ceafb5452572370492a91cb7beefc9df25badd
luciano <luciano.sinisterra@lamercantil.com.ar>
2022-04-25 11:45:48 -0300

    agrego token apim

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -7,25 +7,29 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export class ApiService {
   constructor(private http: HttpClient) {}
 
-  public get(url: string, token?: any) {
-    let headers = new HttpHeaders().set('authorization', `Bearer ${token}`);
+  public get(url: string, tokenRC?: any, tokenAPIM?: any) {
+    let headers = new HttpHeaders().set('authorization', `Bearer ${tokenRC}`);
+    headers = headers.set('Ocp-Apim-Subscription-Key', `${tokenAPIM}`);
     return this.http.get(url, { headers: headers });
   }
 
-  public post(url: string, data: any, token?: any) {
-    let headers = new HttpHeaders().set('authorization', `Bearer ${token}`);
+  public post(url: string, data: any, tokenRC?: any, tokenAPIM?: any) {
+    let headers = new HttpHeaders().set('authorization', `Bearer ${tokenRC}`);
+    headers = headers.set('Ocp-Apim-Subscription-Key', `${tokenAPIM}`);
     data = JSON.parse(data);
     return this.http.post(url, data, { headers: headers });
   }
 
-  public put(url: string, data: any, token?: any) {
-    let headers = new HttpHeaders().set('authorization', `Bearer ${token}`);
+  public put(url: string, data: any, tokenRC?: any, tokenAPIM?: any) {
+    let headers = new HttpHeaders().set('authorization', `Bearer ${tokenRC}`);
+    headers = headers.set('Ocp-Apim-Subscription-Key', `${tokenAPIM}`);
     data = JSON.parse(data);
     return this.http.put(url, data, { headers: headers });
   }
 
-  public delete(url: string, token?: any) {
-    let headers = new HttpHeaders().set('authorization', `Bearer ${token}`);
+  public delete(url: string, tokenRC?: any, tokenAPIM?: any) {
+    let headers = new HttpHeaders().set('authorization', `Bearer ${tokenRC}`);
+    headers = headers.set('Ocp-Apim-Subscription-Key', `${tokenAPIM}`);
 
     return this.http.delete(url, { headers: headers });
   }
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -12,8 +12,9 @@ export class DashboardComponent implements OnInit {
 
   public response: any;
   public responseError: any;
-
   public methods = ['GET', 'POST', 'PUT', 'DELETE'];
+  public fields: any[] = [];
+  public bodyJson = {};
 
   constructor(private apiSVC: ApiService, private FB: FormBuilder) {}
 
@@ -22,8 +23,8 @@ export class DashboardComponent implements OnInit {
     this.form.controls['method'].setValue('GET');
   }
 
-  get(url: string, token?: any) {
-    return this.apiSVC.get(url, token).subscribe(
+  get(url: string, token?: any, tokenApim?: any) {
+    return this.apiSVC.get(url, token, tokenApim).subscribe(
       (data) => {
         this.response = data;
         console.log(data);
@@ -35,22 +36,22 @@ export class DashboardComponent implements OnInit {
     );
   }
 
-  post(url: string, body?: any, token?: any) {
-    return this.apiSVC.post(url, body, token).subscribe((data) => {
+  post(url: string, body?: any, token?: any, tokenApim?: any) {
+    return this.apiSVC.post(url, body, token, tokenApim).subscribe((data) => {
       this.response = data;
       console.log(data);
     });
   }
 
-  put(url: string, body?: any, token?: any) {
-    return this.apiSVC.put(url, body, token).subscribe((data) => {
+  put(url: string, body?: any, token?: any, tokenApim?: any) {
+    return this.apiSVC.put(url, body, token, tokenApim).subscribe((data) => {
       this.response = data;
       console.log(data);
     });
   }
 
-  delete(url: string, token?: any) {
-    return this.apiSVC.delete(url, token).subscribe((data) => {
+  delete(url: string, token?: any, tokenApim?: any) {
+    return this.apiSVC.delete(url, token, tokenApim).subscribe((data) => {
       this.response = data;
       console.log(data);
     });
@@ -61,39 +62,64 @@ export class DashboardComponent implements OnInit {
   }
 
   processRequest() {
+    this.formatJsonRequest();
+
     this.response = '';
     this.responseError = '';
 
     let url = this.form.get('url').value;
-    let token = this.form.get('token').value;
-
+    let token = this.form.get('tokenRc').value;
+    let tokenApim = this.form.get('tokenApim').value;
     let body = this.form.get('body').value;
-
 
     let method = this.form.get('method').value;
 
     switch (method) {
       case 'GET':
-        this.get(url, token);
+        this.get(url, token, tokenApim);
         break;
       case 'POST':
-        this.post(url, body, token);
+        this.post(url, body, token, tokenApim);
         break;
       case 'PUT':
-        this.put(url, body, token);
+        this.put(url, body, token, tokenApim);
         break;
       case 'DELETE':
-        this.delete(url, token);
+        this.delete(url, token, tokenApim);
         break;
     }
+  }
+
+  addField() {
+    if (this.form.get('key').value == null) return;
+    this.fields.push(this.form.get('key').value);
+  }
+
+  formatJsonRequest() {
+    this.bodyJson = {};
+
+    let value = this.form.get('value').value;
+    console.log(value);
+
+    this.fields.forEach((key) => {
+      this.bodyJson += ` ${key}: '' ,`;
+    });
+    console.log(this.bodyJson);
+  }
+
+  removeField(field) {
+    this.fields.splice(this.fields.indexOf(field), 1);
   }
 
   inicializarForm() {
     this.form = this.FB.group({
       url: new FormControl('', [Validators.required]),
-      token: new FormControl(),
+      tokenRC: new FormControl(),
+      tokenAPIM: new FormControl(),
       method: new FormControl('', [Validators.required]),
-      body: new FormControl()
+      body: new FormControl(),
+      key: new FormControl(),
+      value: new FormControl()
     });
   }
 }
