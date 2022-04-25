861ae6fdb179e0097e33e36754feef4a8986122d
luciano <luciano.sinisterra@lamercantil.com.ar>
2022-04-25 12:17:16 -0300

    Fix estilos

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -8,27 +8,27 @@ export class ApiService {
   constructor(private http: HttpClient) {}
 
   public get(url: string, tokenRC?: any, tokenAPIM?: any) {
-    let headers = new HttpHeaders().set('authorization', `Bearer ${tokenRC}`);
+    let headers = new HttpHeaders().set('Authorization', `Bearer ${tokenRC}`);
     headers = headers.set('Ocp-Apim-Subscription-Key', `${tokenAPIM}`);
     return this.http.get(url, { headers: headers });
   }
 
   public post(url: string, data: any, tokenRC?: any, tokenAPIM?: any) {
-    let headers = new HttpHeaders().set('authorization', `Bearer ${tokenRC}`);
+    let headers = new HttpHeaders().set('Authorization', `Bearer ${tokenRC}`);
     headers = headers.set('Ocp-Apim-Subscription-Key', `${tokenAPIM}`);
     data = JSON.parse(data);
     return this.http.post(url, data, { headers: headers });
   }
 
   public put(url: string, data: any, tokenRC?: any, tokenAPIM?: any) {
-    let headers = new HttpHeaders().set('authorization', `Bearer ${tokenRC}`);
+    let headers = new HttpHeaders().set('Authorization', `Bearer ${tokenRC}`);
     headers = headers.set('Ocp-Apim-Subscription-Key', `${tokenAPIM}`);
     data = JSON.parse(data);
     return this.http.put(url, data, { headers: headers });
   }
 
   public delete(url: string, tokenRC?: any, tokenAPIM?: any) {
-    let headers = new HttpHeaders().set('authorization', `Bearer ${tokenRC}`);
+    let headers = new HttpHeaders().set('Authorization', `Bearer ${tokenRC}`);
     headers = headers.set('Ocp-Apim-Subscription-Key', `${tokenAPIM}`);
 
     return this.http.delete(url, { headers: headers });
