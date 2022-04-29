cba6afaf107503b2a993a8136c7b13973aff2561
luciano <luciano.sinisterra@lamercantil.com.ar>
2022-04-28 23:43:58 -0300

    Feature electron agregado

---
+++ main.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require("electron");
+
+let appWin;
+
+createWindow = () => {
+    appWin = new BrowserWindow({
+        width: 800,
+        height: 600,
+        title: "Anman",
+        resizable: true,
+        webPreferences: {
+            contextIsolation: false,
+            nodeIntegration: true
+        }
+    });
+    
+    appWin.loadURL(`file://${__dirname}/dist/index.html`);
+
+    appWin.setMenu(null);
+
+    appWin.webContents.openDevTools();
+
+    appWin.menuBarVisible = true;
+
+    appWin.on("closed", () => {
+        appWin = null;
+    });
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+      app.quit();
+    }
+});
